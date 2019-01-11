34a2018274f781c92f53e90c6375003766b68e2a
Joseph Doyle <j@random-stuff.org>
2019-01-11 15:48:51 -0500

    bitting is annotated on the image now, lines are easier to see, next up clean up and add loops

--- keyBit/keyBit.m
+++ keyBit/keyBit.m
@@ -39,10 +39,10 @@ try
     
     %pixel to inch calibration
     [xblade1,yblade1] = ginput(1)
-    plot(xblade1,yblade1,'g+')
+    plot(xblade1,yblade1,'g+','LineWidth',2)
     [xblade2,yblade2] = ginput(1)
-    plot(xblade2,yblade2,'g+')
-    plot([xblade1 xblade2],[yblade1 yblade2],'g')
+    plot(xblade2,yblade2,'g+','LineWidth',2)
+    plot([xblade1 xblade2],[yblade1 yblade2],'g','LineWidth',2)
     %change in x,y, to pixel distance
     pixWidth = sqrt((xblade1 - xblade2)^2 + (yblade1 - yblade2)^2)
     ppi = pixWidth/bladeWidth
@@ -50,41 +50,41 @@ try
     %loop number of pins
     for i = 1:numPins
         [xtop,ytop] = ginput(1);
-        plot(xtop,ytop,'r+')
+        plot(xtop,ytop,'r+','LineWidth',2)
         [xbot,ybot] = ginput(1);
-        plot(xbot,ybot,'r+')
-        plot([xtop xbot],[ytop ybot],'r')
+        plot(xbot,ybot,'r+','LineWidth',2)
+        plot([xtop xbot],[ytop ybot],'r','LineWidth',2)
         pixWidth = sqrt((xtop - xbot)^2 + (ytop - ybot)^2)
         pix = pixWidth/ppi
         if pix > betRoot01
-            %RGB = insertText(keyImage,[xbot2 ybot2],'0')
+            text(xtop,ytop-50,'0','Color','red','FontSize',25)
             test(i) = 0
         elseif pix > betRoot12
-            %RGB = insertText(keyImage,[xbot2 ybot2],'1')
+            text(xtop,ytop-50,'1','Color','red','FontSize',25)
             test(i) = 1
         elseif pix > betRoot23
-            %RGB = insertText(keyImage,[xbot2 ybot2],'2')
+            text(xtop,ytop-50,'2','Color','red','FontSize',25)
             test(i) = 2
         elseif pix > betRoot34
-            %RGB = insertText(keyImage,[xbot2 ybot2],'3')
+            text(xtop,ytop-50,'3','Color','red','FontSize',25)
             test(i) = 3
         elseif pix > betRoot45
-            %RGB = insertText(keyImage,[xbot2 ybot2],'4')
+            text(xtop,ytop-50,'4','Color','red','FontSize',25)
             test(i) = 4
         elseif pix > betRoot56
-            %RGB = insertText(keyImage,[xbot2 ybot2],'5')
+            text(xtop,ytop-50,'5','Color','red','FontSize',25)
             test(i) = 5
         elseif pix > betRoot67
-            %RGB = insertText(keyImage,[xbot2 ybot2],'6')
+            text(xtop,ytop-50,'6','Color','red','FontSize',25)
             test(i) = 6
         elseif pix > betRoot78
-            %RGB = insertText(keyImage,[xbot2 ybot2],'7')
+            text(xtop,ytop-50,'7','Color','red','FontSize',25)
             test(i) = 7
         elseif pix > betRoot89
-            %RGB = insertText(keyImage,[xbot2 ybot2],'8')
+            text(xtop,ytop-50,'8','Color','red','FontSize',25)
             test(i) = 8
         else
-            %RGB = insertText(keyImage,[xbot2 ybot2],'9')
+            text(xtop,ytop-50,'9','Color','red','FontSize',25)
             test(i) = 9
         end
         
